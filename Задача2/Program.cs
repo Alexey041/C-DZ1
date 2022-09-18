399602157f7153d5c6907920c0634855a754338c
Alex <bazz3k@ya.ru>
2022-09-18 12:17:59 +0300

    Добавил все

---
+++ Задача2/Program.cs
@@ -0,0 +1,38 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт,
+//какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7 min = 5
+//a = 2 b = 10 -> max = 10 min = 2
+//a = -9 b = -3 -> max = -3 min = -9
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+string n1 = Console.ReadLine();
+int number1 = int.Parse(n1);
+Console.Write("Введите второе число: ");
+string n2 = Console.ReadLine();
+int number2 = int.Parse(n2);
+
+if (number1 == number2)
+    {
+        Console.Write("Числа равны");
+    }
+
+else
+    {
+    if (number1 > number2)
+        {
+        Console.Write("max = ");
+        Console.Write(number1);
+        Console.Write(" min = ");
+        Console.Write(number2);
+        }
+    else
+        {
+        Console.Write("max = ");
+        Console.Write(number2);
+        Console.Write(" min = ");
+        Console.Write(number1);
+        }
+
+    }
+
---
+++ Задача4/Program.cs
@@ -0,0 +1,29 @@
+﻿//Напишите программу, которая принимает на вход три числа и 
+//выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+string n1 = Console.ReadLine();
+int number1 = int.Parse(n1);
+Console.Write("Введите второе число: ");
+string n2 = Console.ReadLine();
+int number2 = int.Parse(n2);
+Console.Write("Введите третье число: ");
+string n3 = Console.ReadLine();
+int number3 = int.Parse(n3);
+int max = number1;
+
+if (max < number2)
+    {
+        max = number2;
+    }
+
+if (max < number3)
+    {
+        max = number3;
+    }
+
+Console.Write(max);
